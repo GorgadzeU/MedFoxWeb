a1a27535da41750ff4d7a0a8cc6119363083a94f
Gorgadze <erazerhead12@gmail.com>
2021-10-01 03:01:24 +0400

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className='page home-page'>
+      <header className='home-page__header'>
+        <h1>LOGO</h1>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
