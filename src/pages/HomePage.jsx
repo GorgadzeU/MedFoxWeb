64c6808b909f42a52541350d243206c99b7134d8
Gorgadze <erazerhead12@gmail.com>
2021-10-01 04:45:42 +0400

    Updated home page (DEMO)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './HomePage.css';
+import Logo from '../assets/logo.png';
+import Button from '../components/Button';
 
-const HomePage = () => {
+const HomePage = ({ history }) => {
   return (
     <div className='page home-page'>
       <header className='home-page__header'>
-        <h1>LOGO</h1>
+        <img src={Logo} alt='main logo' />
+        <h1>MEDFOX</h1>
       </header>
+
+      <section className='home-page__info'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quaerat
+        velit libero repudiandae perspiciatis, vel, hic earum explicabo numquam,
+        maxime inventore corrupti excepturi. Vel odit voluptates, commodi veniam
+      </section>
+
+      <div className='home-page__actions'>
+        <Button className='x2-btn' onClick={() => history.push('/patient')}>
+          პაციენტი
+        </Button>
+        <Button className='x2-btn'>თერაპევტი</Button>
+      </div>
     </div>
   );
 };
