3a2ba36528eb3d91cfc03f32c3d0ba0e84c7ec68
Gorgadze <erazerhead12@gmail.com>
2021-10-01 04:44:53 +0400

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 //Import Pages
 import HomePage from './pages/HomePage.jsx';
+import PatientPage from './pages/PatientPage';
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      {/* Declare pages */}
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/patient' exact component={PatientPage} />
+      </Switch>
     </div>
   );
 }
