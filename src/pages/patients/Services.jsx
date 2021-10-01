46f11757c46a9896bf56d8554be82fc266a8811c
Gorgadze <erazerhead12@gmail.com>
2021-10-02 01:58:32 +0400

    Added services section (DEMO)

---
+++ src/pages/patients/Services.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Services.css';
+
+import Card from '../../components/Card';
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+      <Card
+        title='test'
+        content='testtesttesttesttesttesttesttesttesttesttesttesttesttesttest'
+      />
+    </div>
+  );
+};
+
+export default Services;
