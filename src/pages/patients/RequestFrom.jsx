574447a5a91bf7a7822ba75c642081cc7a0afc96
Gorgadze <erazerhead12@gmail.com>
2021-10-05 01:36:39 +0400

    Added send email via emailjs

--- src/pages/patients/RequestFrom.jsx
+++ src/pages/patients/RequestFrom.jsx
@@ -1,24 +1,96 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './RequestForm.css';
 import { Input, TextArea } from '../../components/Input';
+import emailjs from 'emailjs-com';
+import Loading from '../../components/Loading';
+import Button from '../../components/Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RequestFrom = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_rs8p0re',
+        'template_luhct1m',
+        {
+          from_name: name,
+          message: message,
+          from_email: email,
+        },
+        'user_atQz3T86NCTxzC0AEaFvM'
+      )
+      .then(
+        (result) => {
+          // console.log(result.text);
+          setLoading(false);
+          toast.success('Email send successfully');
+        },
+        (error) => {
+          // console.log(error.text);
+          setLoading(false);
+          toast.warning("Could't send email");
+        }
+      );
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <form className='request-from'>
-      <Input
-        type='text'
-        label='Name'
-        id='name-input'
-        onChange={(e) => console.log(e.target.value)}
+    <>
+      {loading && <Loading />}
+      <ToastContainer
+        position='bottom-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
       />
-      <Input
-        type='email'
-        label='Email'
-        id='email-input'
-        onChange={(e) => console.log(e.target.value)}
-      />
-      <TextArea label='Text' onChange={(e) => console.log(e.target.value)} />
-    </form>
+
+      <form ref={form} className='request-from' onSubmit={sendEmail}>
+        <Input
+          type='text'
+          label='Name'
+          id='from_name'
+          name='user_name'
+          value={name}
+          onChange={({ target: { value } }) => setName(value)}
+        />
+        <Input
+          type='email'
+          label='Email'
+          id='from_email'
+          name='user_email'
+          value={email}
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+        <TextArea
+          name='message'
+          label='Text'
+          value={message}
+          onChange={({ target: { value } }) => setMessage(value)}
+        />
+
+        {/* <Input type='submit' value='Send' /> */}
+        <Button type='submit'>Send</Button>
+      </form>
+    </>
   );
 };
 
