bb89a072683052d715d58f2b84e5ed01ec1f700a
Gorgadze <erazerhead12@gmail.com>
2021-10-05 00:18:12 +0400

    Make new NavLink component

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NavLink.css';
+import { Link } from 'react-scroll';
+const NavLink = ({ text, el }) => {
+  return (
+    <Link
+      className='side-nav__link'
+      activeClass='active-side-navLink'
+      to={el}
+      spy={true}
+      smooth={true}
+      offset={-50}
+      duration={500}
+    >
+      <div className='side-nav__link-text'>{text}</div>
+      <span className='side-nav__link-dot'></span>
+      <span className='side-nav__link-line'></span>
+    </Link>
+  );
+};
+
+export default NavLink;
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,50 +1,16 @@
 import React from 'react';
 import './SideNav.css';
-import { Link } from 'react-scroll';
+import NavLink from './NavLink';
 
-const SideNav = () => {
+const SideNav = ({ navLinks }) => {
+  console.log('navlink', navLinks);
   return (
     <div className='side-nav'>
       <div className='side-nav__actions'>
-        <Link
-          className='side-nav__link'
-          activeClass='active-side-navLink'
-          to='el1'
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-        >
-          <div className='side-nav__link-text'>ტერაპევტები</div>
-          <span className='side-nav__link-dot'></span>
-          <span className='side-nav__link-line'></span>
-        </Link>
-        <Link
-          className='side-nav__link'
-          activeClass='active-side-navLink'
-          to='el2'
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-        >
-          <div className='side-nav__link-text'>სერვისები</div>
-          <span className='side-nav__link-dot'></span>
-          <span className='side-nav__link-line'></span>
-        </Link>
-        <Link
-          className='side-nav__link'
-          activeClass='active-side-navLink'
-          to='el3'
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-        >
-          <div className='side-nav__link-text'>მოთხოვნა</div>
-          <span className='side-nav__link-dot'></span>
-          <span className='side-nav__link-line'></span>
-        </Link>
+        {navLinks.length &&
+          navLinks.map((l, i) => {
+            return <NavLink key={i} el={`el${i + 1}`} text={l} />;
+          })}
       </div>
     </div>
   );
