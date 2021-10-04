1f6669b87401f8062dec7e295b74e92b1f776240
Gorgadze <erazerhead12@gmail.com>
2021-10-05 00:18:55 +0400

    Added main nav in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
 const Header = ({ history }) => {
@@ -9,6 +9,22 @@ const Header = ({ history }) => {
       <span className='header__logo' onClick={() => history.push('/')}>
         <img src={Logo} alt='header logo' />
       </span>
+      <nav className='header__nav'>
+        <NavLink
+          to='/patient'
+          className='header__nav-link'
+          activeClassName='header__nav-link-active'
+        >
+          პაციენტი
+        </NavLink>
+        <NavLink
+          to='/therapeut'
+          className='header__nav-link'
+          activeClassName='header__nav-link-active'
+        >
+          თერაპევტი
+        </NavLink>
+      </nav>
     </div>
   );
 };
